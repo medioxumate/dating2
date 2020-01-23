b35050310772f16f40f90c213b2cbd1ec3cca2c0
medioxumate <[briankiehn@comcast.net]>
2020-01-22 19:09:03 -0500

    added previous work from dating1

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created in PhpStorm
+ * @author Brian Kiehn
+ * @date 1/12/2020
+ * @version 1.0
+ * index.php
+ * https://github.com/medioxumate/dating2.git
+ * GreenRiverDev
+ */
+
+//Session
+session_start();
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function(){
+    $view = new Template();
+    echo $view-> render('views/home.html');
+});
+
+//Run Fat-free
+$f3->run();
